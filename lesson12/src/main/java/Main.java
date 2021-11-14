43df5eb9e639a3c3b47153707598f822a4f33edb
ivansugak <iv.minsk@yandex.ru>
2021-11-15 00:00:19 +0300

    Lesson12 - 1-4 задания сделал. 5 буду позже делать. на валидацию пригодится в приложении. но так и не понял как работает System.out.println(str.split("регулярное выражение").length); - строка из задания. должна выводить инт похоже, но как по региксу находить слова в методе split?

---
+++ lesson12/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import com.homework.task1.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String string = "code";
+        Task1 task1 = new Task1();
+        System.out.println(task1.getTwoCharacterFromWord(string));
+    }
+}
