45d01b5cfa5ea7c17a1aba1005fc51225f6d1732
ivansugak <iv.minsk@yandex.ru>
2021-11-23 23:22:30 +0300

    Lesson13 - 5 вроде как работает. не знаю правильно ли понял суть..

--- lesson13/src/main/java/com/homework/task5/Main.java
+++ lesson13/src/main/java/com/homework/task5/Main.java
@@ -1,32 +1,24 @@
 package com.homework.task5;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.homework.hw5.Car;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        try (BufferedReader bis = new BufferedReader(new FileReader("lesson13//src//main//java//com//homework//hw5//car.json"))) {
-            String jsonString = "";
-            String jsonString1;
-            while ((jsonString1 = bis.readLine()) != null) {
-                String s = jsonString1.replaceAll("[_]", "");
-                String t = s.replace("s", "S");
-                String m = t.replace("name", "");
-                jsonString = jsonString + jsonString1;
+        String json = "{\n" +
+                "  \"brand_name\": \"Toyota\",\n" +
+                "  \"model_name\": \"Camry\",\n" +
+                "  \"max_speed\": 230,\n" +
+                "  \"price\": 35000\n" +
+                "}";
+        ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-            }
-
-            StringReader reader = new StringReader(jsonString);
-            ObjectMapper mapper = new ObjectMapper();
-
-            Car car = mapper.readValue(reader, Car.class);
-            System.out.println(car);
-        }
+        com.homework.hw5.Car car = mapper.readValue(json, com.homework.hw5.Car.class);
+        System.out.println(car);
     }
 }
+
+
